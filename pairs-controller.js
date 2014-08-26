1f5947281dc1350a758a559a94e42811d0f731be
Shouko <h999342@gmail.com>
2014-08-26 22:24:10 +0800

    Search using uid instead of fbid to enhance performance

--- pairs-controller.js
+++ pairs-controller.js
@@ -106,9 +106,9 @@ function listAllPairs(logged_in){
 								<td class="pair_table_col_thumbnail1"><a href="https://facebook.com/'+fbid_real1+'" target="_blank"><img src="http://graph.facebook.com/'+ fbid_real1 +'/picture" class="img-responsive img-circle" alt="Thumbnail Image" ></img></a></td> \
 								<td class="pair_table_col_thumbnail2"><a href="https://facebook.com/'+fbid_real2+'" target="_blank"><img src="http://graph.facebook.com/'+ fbid_real2 +'/picture" class="img-responsive img-circle" alt="Thumbnail Image" ></img></a></td> \
 								\
-								<td class="pair_table_col_nama1"><a href="search?fbid='+fbid_real1+'">'+ data['user1']['name'] +'</a></td> \
+								<td class="pair_table_col_nama1"><a href="#search?uid='+data['user1']['uid']+'">'+ data['user1']['name'] +'</a></td> \
 								<td class="pair_table_col_heart"><i class="glyphicon glyphicon-heart heartc"></i></td> \
-								<td class="pair_table_col_name2"><a href="search?fbid='+fbid_real2+'">'+ data['user2']['name'] +'</a></td> \
+								<td class="pair_table_col_name2"><a href="#search?uid='+data['user2']['uid']+'">'+ data['user2']['name'] +'</a></td> \
 								<td class="pair_table_col_vote_count" id="count_'+data['pid']+'">' + data['count'] + '</td> \
 								<td class="pair_table_col_vote_unit">票</td>';
 

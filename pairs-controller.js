0bc5e730e95f4c6ab76b0aa1814ab6f1afa48b6e
Heron Yang <v6510young88@gmail.com>
2014-09-07 23:05:22 +0800

    test comment plugin

--- pairs-controller.js
+++ pairs-controller.js
@@ -172,7 +172,15 @@ function showComment(pid)
                     $('button.share-button').click(function() {
                         shareComment(pid);
                     });
-                    FB.XFBML.parse();
+
+                    (function(d, s, id) {
+                        var js, fjs = d.getElementsByTagName(s)[0];
+                        if (d.getElementById(id)) return;
+                        js = d.createElement(s); js.id = id;
+                        js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&appId=520188428109474&version=v2.0&status=1";
+                        fjs.parentNode.insertBefore(js, fjs);
+                    }(document, 'script', 'facebook-jssdk'));
+                    // FB.XFBML.parse();
 
                     // finally
                     $('#loader-single-gif').hide();

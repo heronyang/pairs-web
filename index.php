44dd68c11ba14799becb9524b9443242b427ec30
Heron Yang <v6510young88@gmail.com>
2014-09-23 10:05:13 +0800

    highlight titles

--- index.php
+++ index.php
@@ -702,7 +702,7 @@ if (in_array($_SERVER['HTTP_USER_AGENT'], array(
                 <div class="modal-content">
                     <div class="modal-header">
                         <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
-                        <h4 class="modal-title">PLAY</h4>
+                        <h4 class="modal-title">PLAY - 匿名投票</h4>
                     </div>
 
                     <div class="modal-body" data-spy="scroll">
